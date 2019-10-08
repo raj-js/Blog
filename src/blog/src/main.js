cf120c0e79ba36c21f94629435b501695d4d5284
Raj <rajesh.js@live.cn>
2019-10-09 00:38:18 +0800

    初始化项目

--- src/blog/src/main.js
+++ src/blog/src/main.js
@@ -1,15 +1,16 @@
-import Vue from 'vue'
-import Ant from 'ant-design-vue'
+import Vue from "vue";
+import ElementUI from "element-ui";
 
-import App from './App.vue'
-import router from './router'
-import 'ant-design-vue/dist/antd.css'
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.use(Ant)
+Vue.use(ElementUI);
 
 new Vue({
   router,
-  render: h => h(App),
-}).$mount('#app')
+  store,
+  render: h => h(App)
+}).$mount("#app");
