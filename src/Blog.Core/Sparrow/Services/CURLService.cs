cbb2ee20525d7f1c1d4a1072e7867689a1ba7aa1
xueguang.zhan <xueguang.zhan@macrowing.com>
2019-10-21 14:45:15 +0800

    Update CURLService.cs

--- src/Blog.Core/Sparrow/Services/CURLService.cs
+++ src/Blog.Core/Sparrow/Services/CURLService.cs
@@ -29,12 +29,12 @@ namespace Blog.Core.Sparrow.Services
         where TKey : IEquatable<TKey>
     {
         protected IMapper Mapper { get; private set; }
-        private readonly ICURLStore<TEntity, TKey> _store;
+        protected ICURLStore<TEntity, TKey> Store { get; private set; }
 
         public CURLService(IMapper mapper, ICURLStore<TEntity, TKey> store)
         {
             Mapper = mapper;
-            _store = store;
+            Store = store;
         }
 
         #region protected
@@ -46,96 +46,96 @@ namespace Blog.Core.Sparrow.Services
 
         public long Count()
         {
-            return _store.Count();
+            return Store.Count();
         }
 
         public Task<long> CountAsync()
         {
-            return _store.CountAsync();
+            return Store.CountAsync();
         }
 
         public TDTO Create(TCreateDTO createDTO)
         {
-            var entity = _store.Create(ToEntity(createDTO));
+            var entity = Store.Create(ToEntity(createDTO));
             return ToDTO(entity);
         }
 
         public TKey CreateAndGetId(TCreateDTO createDTO)
         {
-            return _store.CreateAndGetId(ToEntity(createDTO));
+            return Store.CreateAndGetId(ToEntity(createDTO));
         }
 
         public Task<TKey> CreateAndGetIdAsync(TCreateDTO createDTO)
         {
-            return _store.CreateAndGetIdAsync(ToEntity(createDTO));
+            return Store.CreateAndGetIdAsync(ToEntity(createDTO));
         }
 
         public async Task<TDTO> CreateAsync(TCreateDTO createDTO)
         {
-            var entity = await _store.CreateAsync(ToEntity(createDTO));
+            var entity = await Store.CreateAsync(ToEntity(createDTO));
             return ToDTO(entity);
         }
 
         public void CreateMany(IEnumerable<TCreateDTO> createDTOs)
         {
             var entities = createDTOs.Select(s => ToEntity(s));
-            _store.CreateMany(entities);
+            Store.CreateMany(entities);
         }
 
         public Task CreateManyAsync(IEnumerable<TCreateDTO> createDTOs)
         {
             var entities = createDTOs.Select(s => ToEntity(s));
-            return _store.CreateManyAsync(entities);
+            return Store.CreateManyAsync(entities);
         }
 
         public TDTO Find(TKey id)
         {
-            var entity = _store.Find(id);
+            var entity = Store.Find(id);
             return ToDTO(entity);
         }
 
         public async Task<TDTO> FindAsync(TKey id)
         {
-            var entity = await _store.FindAsync(id);
+            var entity = await Store.FindAsync(id);
             return ToDTO(entity);
         }
 
         public bool Remove(TKey id)
         {
-            return _store.Remove(id);
+            return Store.Remove(id);
         }
 
         public Task<bool> RemoveAsync(TKey id)
         {
-            return _store.RemoveAsync(id);
+            return Store.RemoveAsync(id);
         }
 
         public TDTO Update(TUpdateDTO updateDTO)
         {
             var entity = ToEntity(updateDTO);
-            return ToDTO(_store.Update(entity));
+            return ToDTO(Store.Update(entity));
         }
 
         public TDTO Update(TKey id, params (string Field, object Value)[] selectors)
         {
-            return ToDTO(_store.Update(id, selectors));
+            return ToDTO(Store.Update(id, selectors));
         }
 
         public async Task<TDTO> UpdateAsync(TUpdateDTO updateDTO)
         {
-            var entity = await _store.UpdateAsync(ToEntity(updateDTO));
+            var entity = await Store.UpdateAsync(ToEntity(updateDTO));
             return ToDTO(entity);
         }
 
         public async Task<TDTO> UpdateAsync(TKey id, params (string Field, object Value)[] selectors)
         {
-            var entity = await _store.UpdateAsync(id, selectors);
+            var entity = await Store.UpdateAsync(id, selectors);
             return ToDTO(entity);
         }
 
         public async Task<(List<TDTO> List, long Total)> PageQuery(int pageIndex, int pageSize, params (string Field, bool IsAsc)[] sortFields)
         {
-            var paged = await _store.PageQuery(s => true, sortFields, pageIndex, pageSize);
+            var paged = await Store.PageQuery(s => true, sortFields, pageIndex, pageSize);
 
             return (
                 paged.Entities.Select(s => ToDTO(s)).ToList(),
