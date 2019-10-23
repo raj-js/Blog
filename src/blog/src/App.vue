2e4fdf363f2303f4f3ff72c3e0cac6375673c80f
xueguang.zhan <xueguang.zhan@macrowing.com>
2019-10-23 18:11:49 +0800

    UI调整

--- src/blog/src/App.vue
+++ src/blog/src/App.vue
@@ -11,26 +11,19 @@
 		</v-app-bar>
 
 		<v-navigation-drawer v-model="drawer" app clipped color="grey lighten-4">
-			<v-list dense class="grey lighten-4">
-				<template v-for="(item, i) in menus">
-					<v-row v-if="item.heading" :key="i" align="center">
-						<v-col cols="6">
-							<v-subheader v-if="item.heading">{{ item.heading }}</v-subheader>
-						</v-col>
-						<v-col cols="6" class="text-right">
-							<v-btn small text>edit</v-btn>
-						</v-col>
-					</v-row>
-					<v-divider v-else-if="item.divider" :key="i" dark class="my-4"></v-divider>
-					<v-list-item v-else :key="i">
-						<v-list-item-action>
-							<v-icon>{{ item.icon }}</v-icon>
-						</v-list-item-action>
-						<v-list-item-content>
-							<v-list-item-title class="grey--text">{{ item.text }}</v-list-item-title>
-						</v-list-item-content>
-					</v-list-item>
-				</template>
+			<v-list shaped class="grey lighten-4">
+				<v-list-item-group v-model="menu" color="primary">
+					<template v-for="(item, i) in menus">
+						<v-list-item :key="i">
+							<v-list-item-action>
+								<v-icon>{{ item.icon }}</v-icon>
+							</v-list-item-action>
+							<v-list-item-content>
+								<v-list-item-title class="grey-text">{{ item.text }}</v-list-item-title>
+							</v-list-item-content>
+						</v-list-item>
+					</template>
+				</v-list-item-group>
 			</v-list>
 		</v-navigation-drawer>
 
@@ -52,6 +45,7 @@
 		},
 		data: () => ({
 			drawer: null,
+			menu: 1,
 			menus: [
 				{ icon: "mdi-home", text: "HOME" },
 				{ icon: "mdi-apps", text: "CATEGORY" },
