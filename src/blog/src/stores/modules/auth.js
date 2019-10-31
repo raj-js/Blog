6248beef2617b5f6ce3926ea5d29db78dab91ecd
xueguang.zhan <xueguang.zhan@macrowing.com>
2019-10-31 16:44:34 +0800

    vuex

---
+++ src/blog/src/stores/modules/auth.js
@@ -0,0 +1,20 @@
+export default {
+    state: {
+        token: '',
+        profile: {}
+    },
+    getters: {
+        
+    },
+    mutations: {
+
+    },
+    actions: {
+        signIn() {
+
+        },
+        signOut() {
+
+        }
+    }
+}
